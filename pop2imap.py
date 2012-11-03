8e9f8923d06df8a6a9744bfe40cd7da2b0f00153
Roy Hyunjin Han <starsareblueandfaraway@gmail.com>
2012-11-03 15:48:19 -0600

    Added PORT binding for use with crontab

--- pop2imap.py
+++ pop2imap.py
@@ -1,28 +1,49 @@
+import os
 from email import message_from_string
 from imapIO import IMAP4_SSL
 from poplib import POP3_SSL
+from socket import socket
 
 
-SOURCE_USERNAME = 'user@hotmail.com'
+SOCKET = socket()
+PORT = 9000
+SOURCE_USERNAME = 'username@hotmail.com'
 SOURCE_PASSWORD = 'password'
-TARGET_USERNAME = 'user@gmail.com'
+TARGET_USERNAME = 'username@gmail.com'
 TARGET_PASSWORD = 'password'
 
 
-print 'Connecting to POP3...'
-sourceServer = POP3_SSL('pop3.live.com')
-sourceServer.user(SOURCE_USERNAME)
-sourceServer.pass_(SOURCE_PASSWORD)
-print 'Connecting to IMAP4...'
-targetServer = IMAP4_SSL.connect(
-    'imap.gmail.com',
-    993,
-    TARGET_USERNAME,
-    TARGET_PASSWORD)
-print 'Counting messages on POP3...'
-messageCount = len(sourceServer.list()[1])
-for messageIndex in xrange(1, 1 + messageCount):
-    print 'Reviving %s / %s' % (messageIndex, messageCount)
-    message = message_from_string('\r\n'.join(sourceServer.retr(messageIndex)[1]))
-    message['date'] = message['Date']
-    targetServer.revive('Inbox', message)
+def run():
+    try:
+        SOCKET.bind(('', PORT))
+    except socket.error:
+        return
+    print 'Connecting to POP3...'
+    sourceServer = POP3_SSL('pop3.live.com')
+    sourceServer.user(SOURCE_USERNAME)
+    sourceServer.pass_(SOURCE_PASSWORD)
+    print 'Connecting to IMAP4...'
+    targetServer = IMAP4_SSL.connect(
+        'imap.gmail.com',
+        993,
+        TARGET_USERNAME,
+        TARGET_PASSWORD)
+    print 'Counting messages on IMAP4...'
+    messageCount = len(sourceServer.list()[1])
+    messagePath = os.path.splitext(__file__)[0] + '.txt'
+    try:
+        messageIndexStart = int(open(messagePath).read()) + 1
+    except IOError:
+        messageIndexStart = 1
+    if messageIndexStart > messageCount:
+        return
+    for messageIndex in xrange(messageIndexStart, messageCount + 1):
+        print 'Reviving %s / %s' % (messageIndex, messageCount)
+        message = message_from_string('\r\n'.join(sourceServer.retr(messageIndex)[1]))
+        message['date'] = message['Date']
+        targetServer.revive('Inbox', message)
+        open(messagePath, 'wt').write(str(messageIndex))
+
+
+if __name__ == '__main__':
+    run()
